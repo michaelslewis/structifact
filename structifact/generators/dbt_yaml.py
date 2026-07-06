f6ff5ac77bda6b49bc57178be7d90e1572193f29
Michael S. Lewis <calmike@berkeley.edu>
2026-07-06 00:52:41 -0700

    Add adapter architecture with YAML and Excel input support

--- structifact/generators/dbt_yaml.py
+++ structifact/generators/dbt_yaml.py
@@ -1,23 +1,29 @@
+from .base import Generator, Artifact
 import yaml
-
 from ..ir import TableSpec
 
 
-def generate_dbt_yaml(table: TableSpec) -> str:
-    data = {
-        "version": 2,
-        "models": [
-            {
-                "name": table.name,
-                "columns": [
-                    {
-                        "name": f.name,
-                        "description": f.description or ""
-                    }
-                    for f in table.fields
-                ],
-            }
-        ],
-    }
+class DBTYAMLGenerator(Generator):
+    name = "dbt"
 
-    return yaml.dump(data, sort_keys=False)
+    def generate(self, table: TableSpec) -> Artifact:
+        data = {
+            "version": 2,
+            "models": [
+                {
+                    "name": table.name,
+                    "columns": [
+                        {
+                            "name": f.name,
+                            "description": f.description or ""
+                        }
+                        for f in table.fields
+                    ],
+                }
+            ],
+        }
+
+        return Artifact(
+            filename=f"{table.name}.yml",
+            content=yaml.dump(data, sort_keys=False)
+        )
