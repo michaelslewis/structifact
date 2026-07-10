494efcb636757fe7a8f5baff874e05c71f34cdcc
Michael S. Lewis <calmike@berkeley.edu>
2026-07-09 23:40:54 -0700

    Integrate type normalization into YAML adapter

--- structifact/adapters/yaml.py
+++ structifact/adapters/yaml.py
@@ -1,20 +1,30 @@
 import yaml
 
 from ..ir import TableSpec, FieldSpec
+from ..types import parse_type
 
 
 def load_yaml(path: str) -> TableSpec:
     with open(path, "r") as f:
         data = yaml.safe_load(f)
 
-    fields = [
-        FieldSpec(
-            name=field["name"],
-            type=field["type"],
-            description=field.get("description"),
+    fields = []
+
+    for field in data["fields"]:
+        parsed = parse_type(field["type"])
+
+        fields.append(
+            FieldSpec(
+                name=field["name"],
+                type=parsed["type"],
+                raw_type=field["type"],
+                description=field.get("description"),
+
+                length=parsed.get("length"),
+                precision=parsed.get("precision"),
+                scale=parsed.get("scale"),
+            )
         )
-        for field in data["fields"]
-    ]
 
     return TableSpec(
         name=data["table"],
